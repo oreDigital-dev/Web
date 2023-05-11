3df17a3242201547651dff00960a584dd31e3f9e
Majura Anthony Gaudence <andersonmia1968@gmail.com>
2023-05-11 15:11:06 +0200

    Update serviceComp.tsx
    Adding height and width attributes to ServiceComp

--- apps/web/src/screens/home/components/serviceComp.tsx
+++ apps/web/src/screens/home/components/serviceComp.tsx
@@ -3,7 +3,7 @@ const ServiceComp = (props) =>{
 
     return(
         <div>
-            <Image src={props.image} alt="Incident Repoting" className=""  />
+            <Image src={props.image} alt="Incident Repoting" className=""  height={200} width={200} />
             <div>
                 <p className="font-[700]"> {props.title} </p>
                 <p> {props.content} </p>
