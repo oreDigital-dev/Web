3ead6e986dbde3527acbe3841cfe9ce3ea4ad817
Majura Anthony Gaudence <andersonmia1968@gmail.com>
2023-05-11 19:13:15 +0200

    test comment

--- apps/web/src/screens/home/components/serviceComp.tsx
+++ apps/web/src/screens/home/components/serviceComp.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 const ServiceComp = (props) =>{
 
+    // test comment
+
     return(
         <div>
             <Image src={props.image} alt="Incident Repoting" className=""  height={200} width={200} />
