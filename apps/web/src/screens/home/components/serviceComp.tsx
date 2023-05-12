0d58709dce3f4eab3f5678f78a787c25246ba816
Majura Anthony Gaudence <andersonmia1968@gmail.com>
2023-05-12 10:05:23 +0200

    [feat]: styling services

--- apps/web/src/screens/home/components/serviceComp.tsx
+++ apps/web/src/screens/home/components/serviceComp.tsx
@@ -4,12 +4,12 @@ const ServiceComp = (props) =>{
 
 
     return(
-        <div>
-            <Image src={props.image} alt="Incident Repoting" className=""  height={200} width={200} />
-            <div>
-                <p className="font-[700]"> {props.title} </p>
+        <div className="rounded-lg  w-full border border-b-[#F0F0F0]">
+            <Image src={props.image} alt="Incident Repoting" height={400} width={800} layout="responsive"  className="w-full object-cover"/>
+            <div className="px-8 py-4">
+                <p className="font-[600]"> {props.title} </p>
                 <p> {props.content} </p>
-                <button className="rounded-md">Learn more</button>
+                <button className="rounded-md bg-[#F5F5F5] px-8 py-2 hover:bg-black hover:text-white">Learn more</button>
             </div>
         </div>
     )
--- apps/web/src/screens/home/services.tsx
+++ apps/web/src/screens/home/services.tsx
@@ -1,22 +1,36 @@
 import ServiceComp from "./components/serviceComp";
-import IncidentOne from '../../../assets/incident1.jpg'
-import IncidentTwo from '../../../assets/incident2.jpg'
-import IncidentThree from '../../../assets/incident3.jpg'
+import IncidentOne from "../../../assets/incident1.jpg";
+import IncidentTwo from "../../../assets/incident2.jpg";
+import IncidentThree from "../../../assets/incident3.jpg";
 
 /* eslint-disable react/no-unescaped-entities */
 const Services = () => {
-    const header = "Incident reporting system installation"
-    const paragraph = "Here's the services that we offer to you, to ensure both your company's products and employees security"
+  const header = "Incident reporting system installation";
+  const paragraph =
+    "Here's the services that we offer to you, to ensure both your company's products and employees security";
   return (
-    <div className="px-24">
-      <p className="font-[700px] text-xl text-center">Our Services</p>
-      <p className="text-center">
-        {paragraph}
-      </p>
-      <div className="flex">
-        <ServiceComp image={IncidentOne.src} title={header} content={paragraph} />
-        <ServiceComp image={IncidentTwo.src} title={header} content={paragraph} />
-        <ServiceComp image={IncidentThree.src} title={header} content={paragraph} />
+    <div className="px-32 flex flex-col">
+      <div className="m-auto flex flex-col items-center w-[32%]">
+        <p className="font-[700] text-4xl">Our Services</p>
+        <p className="text-center">{paragraph}</p>
+      </div>
+
+      <div className="flex justify-between gap-12 mt-12">
+        <ServiceComp
+          image={IncidentOne.src}
+          title={header}
+          content={paragraph}
+        />
+        <ServiceComp
+          image={IncidentTwo.src}
+          title={header}
+          content={paragraph}
+        />
+        <ServiceComp
+          image={IncidentThree.src}
+          title={header}
+          content={paragraph}
+        />
       </div>
     </div>
   );
