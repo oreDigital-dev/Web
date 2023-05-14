36fbc78089c8f518d6f2e18badcce3af7e507e6b
ndzhwr <ndizihiweregis06@gmail.com>
2023-05-14 13:22:50 +0200

    [web/fix]: impact erros fixed

--- apps/web/src/screens/home/components/impact.card.tsx
+++ apps/web/src/screens/home/components/impact.card.tsx
@@ -9,9 +9,9 @@ interface ImpactProps {
 
 const ImpactCard: FC<ImpactProps> = (props: ImpactProps) => {
     return (
-        <div className="bg-white p-6 rounded-lg flex flex-col gap-4 shadow-md shadow-slate-200">
-            <img src={props.image} alt="" />
-            <h6>{props.title}</h6>
+        <div className="bg-white p-6 rounded-lg flex flex-col gap-4 shadow-lg shadow-slate-100">
+            <img src={props.image} alt="" className="w-24" />
+            <h6 className="text-lg font-semibold text-gray-900">{props.title}</h6>
             <p>{props.desc}</p>
         </div>
     )
--- apps/web/src/screens/home/impact.tsx
+++ apps/web/src/screens/home/impact.tsx
@@ -1,7 +1,7 @@
 import Impact1 from '../../../assets/vectors/impact1.svg'
 import Impact2 from '../../../assets/vectors/impact2.svg'
 import Impact3 from '../../../assets/vectors/impact3.svg'
-
+import ImpactCard from './components/impact.card'
 const data = [
     {
         title: 'Poweful Incident reporting system at the site',
@@ -22,14 +22,14 @@ const data = [
 ]
 export default function Impact() {
     return (
-        <div className="px-[5vw] text-center py-48">
+        <div className="px-[5vw]  py-48 bg-gradient-to-b from-yellow-50  to-yellow-0">
             <p className="text-6xl text-center font-extrabold ">Our Impact  </p>
-            <p className="text-slate-300">Here’s  the services that we offer to you, to ensure both your company’s
+            <p className="text-slate-300 my-6 text-center">Here’s  the services that we offer to you, to ensure both your company’s
                 products and employees security</p>
-            <div className="grid grid-cols-3">
+            <div className="grid grid-cols-3 gap-10">
                 {
                     data.map((datum, index) => (
-                        <Impact key={index} {...datum} />
+                        <ImpactCard key={index} {...datum} />
                     ))
                 }
             </div>
