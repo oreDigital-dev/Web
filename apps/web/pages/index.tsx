4ffaeac16d354982e41d27e02dee627e759ec115
ndzhwr <ndizihiweregis06@gmail.com>
2023-05-14 15:04:34 +0200

    [web/feat]: reviews

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -4,6 +4,7 @@ import Hero from "../src/screens/home/hero";
 import Services from "../src/screens/home/services";
 import Team from "../src/screens/home/team";
 import Impact from "../src/screens/home/impact";
+import AboutUs from "../src/screens/home/aboutus";
 
 export default function Web() {
   const { data } = useHelloQuery();
@@ -14,6 +15,7 @@ export default function Web() {
       <Services />
       <Team />
       <Impact />
+      <AboutUs />
     </>
   );
 }
